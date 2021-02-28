2ce93c8b3e3be8e3c079267c5e5b8f1a1c2e6f35
sahacingzet <79780060+sahacingzet@users.noreply.github.com>
2021-02-28 13:06:49 +0700

    Add files via upload

---
+++ Lib/developer.js
@@ -0,0 +1,71 @@
+const developer = (prefix) => { 
+	return `「 *AINE BOT* 」
+
+◪ *INFO DEVELOPER*
+  ❏ Nama: Muhammad Ridwan Reynaldy
+  ❏ Wa: wa.me/62895330379186
+  ❏ Ig: https://instagram.com/anemio999
+  ❏ Fb: https://facebook.com/ridwan228
+  ----------------------------------
+◪ *INFO AINEBOT*
+  ❏ Nama: AINEBOT 
+  ❏ Nomor: wa.me/12195159558
+  ❏ Fb: https://facebook.com/ainneboot
+  ----------------------------------
+            *Rules* - *Simple*
+▬▭▬▭▬▭▬▭▬▭▬▭▬
+  ❏ *Spam : Auto Block!*
+  ❏ *Beri Jeda 10detik Saat Menggunakannya!!*
+  ❏ *Bug/Error Harap Cht Owner!*
+  ❏ *Untuk Memastikan Bot Off Atau On*
+  ❏ *Ketik ${prefix}bot*
+  ❏ *Untuk Melihat Sisa Limit Kamu*
+  ❏ *Ketik ${prefix}limit*
+  ❏ *Harap Sabar Dengan Bug²nya!*
+  ❏ *Gunakan Bot Sebaik-baiknya!*
+▬▭▬▭▬▭▬▭▬▭▬▭▬
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}snk
+  ├─ ❏ ${prefix}lpr 
+  ├─ ❏ ${prefix}request
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}ping
+  ├─ ❏ ${prefix}buypremium
+  ├─ ❏ ${prefix}hargaprem
+  └─ ❏ ${prefix}gcainebot     
+◪ *OWNER*
+  │
+  ├─ ❏ ${prefix}setprefix
+  ├─ ❏ ${prefix}setreply
+  ├─ ❏ ${prefix}setlimit
+  ├─ ❏ ${prefix}addlimit
+  ├─ ❏ ${prefix}giftlimit
+  ├─ ❏ ${prefix}setmemlimit
+  ├─ ❏ ${prefix}setppbot
+  ├─ ❏ ${prefix}premium
+  ├─ ❏ ${prefix}unpremium
+  ├─ ❏ ${prefix}admin
+  ├─ ❏ ${prefix}unadmin
+  ├─ ❏ ${prefix}ban
+  ├─ ❏ ${prefix}unban
+  ├─ ❏ ${prefix}bc
+  ├─ ❏ ${prefix}block
+  ├─ ❏ ${prefix}unblock
+  ├─ ❏ ${prefix}setprefix
+  ├─ ❏ ${prefix}event [1/0]
+  ├─ ❏ ${prefix}clone
+  └─ ❏ ${prefix}clearall
+◪ *ADMIN*
+  │
+  ├─ ❏ ${prefix}event
+  ├─ ❏ ${prefix}bcc
+  ├─ ❏ ${prefix}bann
+  ├─ ❏ ${prefix}unbann
+  ├─ ❏ ${prefix}blockk
+  ├─ ❏ ${prefix}unblockk
+  └─ ❏ ${prefix}giftlimit`
+}
+
+exports.developer = developer
---
+++ Lib/donasi.js
@@ -0,0 +1,10 @@
+const donasi = () => { 
+	return `       
+
+◪ *DONASI*
+  │
+  ├─ ❏ *Pulsa*
+  └─ ❏ 0895-3303-79186`
+}
+
+exports.donasi = donasi
